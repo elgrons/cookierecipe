1d56ec383b37b095cf06a2b4aabed74c19f4b4d4
Ashe Urban <ashe@goldentongue.com>
2023-01-11 11:32:42 -0800

    ADD a lightMode Button.
    Co-authored-by: Mitchell Gantz <mgantz22@icloud.com>
    Co-Authored by: Kaleb Taylor
    <kaleb.taylor1976@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,17 @@
 window.onload = function() {
 
   let body = document.body;
-  let darkButton = document.querySelector("button");
+  let darkButton = document.querySelector(".dark");
+  let lightButton = document.querySelector(".light");
 
   darkButton.onclick = function() {
     body.style.backgroundColor = "black"; 
     body.style.color = "white";
+  };
+
+  lightButton.onclick = function() {
+    body.style.backgroundColor = "white";
+    body.style.color = "black";
   };
 
 };
